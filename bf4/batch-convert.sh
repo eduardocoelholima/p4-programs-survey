a4b141899b017029c4472b0c27816aa49159ec99
eduardocoelholima <eduardocoelholima@gmail.com>
2021-12-14 18:46:23 -0500

    converted files to p4-16

---
+++ bf4/batch-convert.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+for i in $@
+do
+    ./convert.sh $i
+done
---
+++ bf4/etc/do-p4c-14.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+echo "usage: do2.sh"
+
+export BUILD_DIR='build'
+export PCAP_DIR='pcaps'
+export LOG_DIR='logs'
+
+if [ "$#" -eq 0 ]; then
+  echo "0 args"
+  export basename='example1'
+else
+  export basename=$1
+fi
+
+mkdir -p $BUILD_DIR $PCAP_DIR $LOG_DIR
+export P4C='p4c-bm2-ss'
+# P4C_ARGS += --p4runtime-files $(BUILD_DIR)/$(basename $@).p4.p4info.txt
+export P4C_ARGS='--p4runtime-files $BUILD_DIR/$basename.p4.p4info.txt'
+# $P4C --p4v 16 $P4C_ARGS -o $BUILD_DIR/$@ $<
+# $P4C --p4v 16 $P4C_ARGS -o $BUILD_DIR $basename.p4
+$P4C --p4v 14 $basename.p4 -o $basename.json
+
