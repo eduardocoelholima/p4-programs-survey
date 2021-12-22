32a1b1249f27fecbf14ce6d4afe2b0b8a1d2f118
eduardocoelholima <eduardocoelholima@gmail.com>
2021-12-22 15:21:01 -0500

    removed directory etc/ and moved files to bf4/

---
+++ bf4/do-bf4.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# this is needed a single time
+#make cptemplate
+
+# python3 ../sigcomm-2020/cleanup_v1.py simple_nat.p4
+# p4c-analysis ./simple_nat-integrated.p4 > simple_nat.log
+
+# python3 ../sigcomm-2020/cleanup_v1.py running.p4 
+# p4c-analysis ./running-integrated.p4 > running.log
+
+# python3 ../sigcomm-2020/cleanup_v1.py basic.p4
+# p4c-analysis ./basic-integrated.p4 > basic.log
+
+if [ "$#" -eq 0 ]; then
+  echo "0 args"
+  export basename='example1'
+else
+  export basename=$1
+fi
+
+rm -rf /home/ecl7037/code/p4-programs-survey/bf4/results/$basename.log /home/ecl7037/code/p4-programs-survey/bf4/results/$basename.err
+cd /home/ecl7037/code/bf4/build/
+python3 /home/ecl7037/code/bf4/sigcomm-2020/cleanup_v1.py /home/ecl7037/code/p4-programs-survey/bf4/$basename.p4 >> /home/ecl7037/code/p4-programs-survey/bf4/results/$basename.log 2>>/home/ecl7037/code/p4-programs-survey/bf4/results/$basename.err
+p4c-analysis ./$basename-integrated.p4 >> /home/ecl7037/code/p4-programs-survey/bf4/results/$basename.log 2>>/home/ecl7037/code/p4-programs-survey/bf4/results/$basename.err
+cp /home/ecl7037/code/bf4/build/$basename* /home/ecl7037/code/p4-programs-survey/bf4/instrumentation/
